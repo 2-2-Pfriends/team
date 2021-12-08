acce756e4909073fea0fc8b6d6ee102aaefc8768
JeungWookEun <e_nfoo@naver.com>
2021-12-08 21:13:57 +0900

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Register', url: '/register', icon: 'person-add' },
+    { title: 'Login', url: '/login', icon: 'enter' },
+    { title: 'Dashboard', url: '/dashboard', icon: 'card' },
+    { title: 'Profile', url: '/profile', icon: 'body' },
+ ];
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  HttpClientModule, FormsModule, JwtModule.forRoot({
+    config: {
+      tokenGetter: () => localStorage.getItem('authToken'),
+    },
+  }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
